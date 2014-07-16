b71e24877f942c74e7492f8ec16902de63235ade
bhagyashree <bbaggi@tavisca.com>
2014-07-16 07:03:31 +0530

    Added 5 test cases

--- Bowling/Game.cs
+++ Bowling/Game.cs
@@ -8,15 +8,41 @@ namespace Bowling
 {
     public class Game
     {
+        int[] frame = new int[20];
+        int rolls = 0;
+        int score = 0;
         public void Roll(int pins)
         {
-            throw new NotImplementedException();
+            if (pins == 10)
+            {
+                frame[rolls++] = pins;
+                frame[rolls++] = 0;
+            }
+            else frame[rolls++] = pins;
+            //Console.WriteLine("frame[{0}]={1}",i,frame[i]);
         }
 
         public int GetScore()
         {
-            throw new NotImplementedException();
-        }
+            int j = 0;
+            for (j = 0; j < 20; j++)
+            {
+                if ((j % 2 == 0) && (frame[j] == 10) && (frame[j+2]==10))
+                    score = score + frame[j+2]+frame[j+4];
+                else if ((j % 2 == 0) && (frame[j + 2] == 10))
+                    score = score + frame[j + 2] + frame[j + 3];
+                else if ((j%2==0) && ((frame[j]+frame[j+1])==10))
+                    score=score+frame[j+2];
+                
+                
+                score=score+frame[j];
+            }
+            return score;
+           
 
+        }
     }
 }
+
+    
+
--- BowlingFixtures/GameFixture.cs
+++ BowlingFixtures/GameFixture.cs
@@ -5,10 +5,182 @@ namespace BowlingFixtures
 {
     [TestClass]
     public class GameFixture
-    {
+    {  
+    /// <summary>
+    ///This is a test class for GameTest and is intended
+    ///to contain all GameTest Unit Tests
+    ///</summary>
+     
+        //1.check when all pins are strikes
+        //2.check when all pins are spares
+        //3.check when all pins are gutter
+        //4.check when some inputs are strikes and some are spare
+        //5.case with score in between
+
+        //1.check when all pins are strikes
         [TestMethod]
-        public void TestMethod1()
+        public void GameWithAllStrikes()
         {
+
+            try
+            {
+                Game gmobj = new Game();
+                gmobj.Roll(10);
+                gmobj.Roll(10);
+                gmobj.Roll(10);
+                gmobj.Roll(10);
+                gmobj.Roll(10);
+                gmobj.Roll(10);
+                gmobj.Roll(10);
+                gmobj.Roll(10);
+                gmobj.Roll(10);
+                gmobj.Roll(10);
+                gmobj.Roll(10);
+                gmobj.Roll(10);
+               int score= gmobj.GetScore();
+                Assert.AreEqual(score,300);
+
+            }
+            catch (Exception ex)
+            {
+                //Assert.Fail("Not equal to {0}!!!",ex.GetType().Name);
+            }
         }
+
+        //2.check when all pins are gutter
+        [TestMethod]
+        public void GameWithAllGutters()
+        {
+
+            try
+            {
+                Game gmobj = new Game();
+                gmobj.Roll(0);
+                gmobj.Roll(0);
+                gmobj.Roll(0);
+                gmobj.Roll(0);
+                gmobj.Roll(0);
+                gmobj.Roll(0);
+                gmobj.Roll(0);
+                gmobj.Roll(0);
+                gmobj.Roll(0);
+                gmobj.Roll(0);
+                gmobj.Roll(0);
+                gmobj.Roll(0);
+                gmobj.Roll(0);
+                gmobj.Roll(0);
+                gmobj.Roll(0);
+                gmobj.Roll(0);
+                gmobj.Roll(0);
+                gmobj.Roll(0);
+                gmobj.Roll(0);
+                int score = gmobj.GetScore();
+                Assert.AreEqual(score, 0);
+
+            }
+            catch (Exception ex)
+            {
+                //Assert.Fail("Not equal to {0}!!!",ex.GetType().Name);
+            }
+        }
+
+
+
+
+        //3.check when all pins are spare
+        [TestMethod]
+        public void GameWithAllSpares()
+        {
+
+            try
+            {
+                Game gmobj = new Game();
+                gmobj.Roll(3);
+                gmobj.Roll(7);
+                gmobj.Roll(3);
+                gmobj.Roll(7);
+                gmobj.Roll(3);
+                gmobj.Roll(7);
+                gmobj.Roll(3);
+                gmobj.Roll(7);
+                gmobj.Roll(3);
+                gmobj.Roll(7);
+                gmobj.Roll(3);
+                gmobj.Roll(7);
+                gmobj.Roll(3);
+                gmobj.Roll(7);
+                gmobj.Roll(3);
+                gmobj.Roll(7);
+                gmobj.Roll(3);
+                gmobj.Roll(7);
+                gmobj.Roll(3);
+                gmobj.Roll(7);
+                int score = gmobj.GetScore();
+                Assert.AreEqual(score, 127);
+
+            }
+            catch (Exception ex)
+            {
+                //Assert.Fail("Not equal to {0}!!!",ex.GetType().Name);
+            }
+        }
+
+
+        //4.check when all pins are strike or spare
+        [TestMethod]
+        public void GameWithSomeSparesSomeStrikes()
+        {
+
+            try
+            {
+                Game gmobj = new Game();
+                gmobj.Roll(10);
+                gmobj.Roll(10);
+                gmobj.Roll(3);
+                gmobj.Roll(7);
+                gmobj.Roll(10);
+                gmobj.Roll(10);
+                gmobj.Roll(10);
+                gmobj.Roll(10);
+                gmobj.Roll(10);
+                gmobj.Roll(10);
+                gmobj.Roll(10);
+                gmobj.Roll(10);
+                gmobj.Roll(10);
+                int score = gmobj.GetScore();
+                Assert.AreEqual(score, 283);
+
+            }
+            catch (Exception ex)
+            {
+                //Assert.Fail("Not equal to {0}!!!",ex.GetType().Name);
+            }
+        }
+
+        //5.case with score in between
+        [TestMethod]
+        public void GameWithSomeSparesSomeStrikes()
+        {
+
+            try
+            {
+                Game gmobj = new Game();
+                gmobj.Roll(10);
+                gmobj.Roll(10);
+                gmobj.Roll(3);
+                gmobj.Roll(7);
+               
+                int score = gmobj.GetScore();
+                Assert.AreEqual(score, 53);
+
+            }
+            catch (Exception ex)
+            {
+                //Assert.Fail("Not equal to {0}!!!",ex.GetType().Name);
+            }
+        }
+
+
+      
     }
 }
